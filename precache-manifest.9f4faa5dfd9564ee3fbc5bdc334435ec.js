306a320b8f7e9d73af957077f60afa79d8711e66
Yevgeniy <zleonenkov@gmail.com>
2019-07-30 16:33:48 +0300

    Updates

---
+++ precache-manifest.9f4faa5dfd9564ee3fbc5bdc334435ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29de00211b04cfa39fcee19755cbbd8b",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "7fbd0d4afee8f1378a5e",
+    "url": "/react_tabs/static/css/main.2ee5ce24.chunk.css"
+  },
+  {
+    "revision": "a816567f6997c7470b61",
+    "url": "/react_tabs/static/js/2.eff7d8e5.chunk.js"
+  },
+  {
+    "revision": "7fbd0d4afee8f1378a5e",
+    "url": "/react_tabs/static/js/main.583a0822.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
